c84fdbdf1b4ca7c139e8abfc05397a4747386262
Sabbyosachy <subbyosachychakraborty@gmail.com>
2021-11-01 09:51:00 +0600

    change

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const ObjectId=require('mongodb').ObjectId;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fvc1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fvc1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 async function run(){
   try {
